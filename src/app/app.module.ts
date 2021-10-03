c23b0200b707c92f092edb499c08ff62f0ce9fa8
iNerdGirl <autumn@inerdgirl.com>
2021-10-02 21:38:53 -0600

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GstAddComponent,
+    GstGetComponent,
+    GstEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gst-get/gst-get.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GstGetComponent } from './gst-get.component';
+
+describe('GstGetComponent', () => {
+  let component: GstGetComponent;
+  let fixture: ComponentFixture<GstGetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GstGetComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GstGetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
